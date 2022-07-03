b7b71ed149f2248b0dd51d81b459d087d23a0194
Javier Candeira <javi.candeira@gmail.com>
2022-07-03 10:49:28 +0200

    login page

--- BPR-Blazor/Models/Professor.cs
+++ BPR-Blazor/Models/Professor.cs
@@ -2,5 +2,8 @@
 {
     public class Professor : User
     {
+        public Professor(string username, string password, string name, string email, DateTime birthday, Chat[] chats) : base(username, password, name, email, birthday, chats)
+        {
+        }
     }
 }
