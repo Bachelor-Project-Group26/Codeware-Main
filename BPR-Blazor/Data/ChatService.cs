3b48cda0a82a127d8e618b8e75cbae3d102c9733
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2022-09-30 11:06:27 +0200

    Update ChatService.cs

--- BPR-Blazor/Data/ChatService.cs
+++ BPR-Blazor/Data/ChatService.cs
@@ -6,7 +6,7 @@ namespace BPR_Blazor.Data
 {
     public class ChatService : IChatService
     {
-        private string URL = "http://localhost:7000";
+        private string URL = "https://localhost:7000";
 
         public async Task<string> GetChat(string Username, int Id)
         {
